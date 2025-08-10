632ab4dc83720d9690fd9ba740bfcfd941875b3b
lochan <lochan1917.com>
2025-08-10 17:35:50 +0530

    added fevicon and name

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,10 @@ export const metadata: Metadata = {
   generator: 'Next.js',
   icons: {
     icon: '/favicon.ico',
+    shortcut: '/favicon-16x16.png',
+    apple: '/apple-touch-icon.png',
   },
+  manifest: '/site.webmanifest',
 }
 
 export default function RootLayout({
@@ -20,6 +23,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
         <style>{`
 html {
   font-family: ${GeistSans.style.fontFamily};
